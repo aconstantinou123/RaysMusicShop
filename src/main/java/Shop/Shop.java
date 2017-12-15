a0ec377a088194560bd22c685d5b53a3d4c1c6ec
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-12-15 17:06:18 +0000

    can can calculate total buy/sell/markup prices for whole shop

--- src/main/java/Shop/Shop.java
+++ src/main/java/Shop/Shop.java
@@ -1,4 +1,62 @@
 package Shop;
 
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+        this.name = name;
+        this.stock = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public void addStock(ISell item) {
+        stock.add(item);
+    }
+
+    public void removeStock(ISell item) {
+        stock.remove(item);
+    }
+
+    public int numberOfItemsInStock() {
+        return stock.size();
+    }
+
+    public double calculateTotalPotentialProfit(){
+        double profit = 0;
+        for (ISell item : stock) {
+            profit += item.calculateMarkup();
+        }
+        return profit;
+    }
+
+    public double calculateTotalBuyPrice(){
+        double buyPrice = 0;
+        for (ISell item : stock) {
+            buyPrice += item.getBuyPrice();
+        }
+        return buyPrice;
+    }
+
+    public double calculateTotalSellPrice(){
+        double sellPrice = 0;
+        for (ISell item : stock) {
+            sellPrice += item.getSellPrice();
+        }
+        return sellPrice;
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -5,6 +5,8 @@ import Drums.TomTomDrum;
 import Instruments.*;
 import MiscItems.DrumStick;
 import MiscItems.GuitarString;
+import MiscItems.SheetMusic;
+import Shop.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,13 +18,20 @@ public class ShopTest {
     GuitarString guitarString;
 
     BassDrum bassDrum;
+    BassDrum bassDrum2;
     Cymbal cymbal;
+    Cymbal cymbal2;
     Snare snare;
+    Snare snare2;
     TomTomDrum tomTomDrum;
+    TomTomDrum tomTomDrum2;
     DrumStick drumStick;
     DrumKit drumKit;
+    SheetMusic sheetMusic;
 
     KeyBoard keyBoard;
+
+    Shop shop;
 
 
     @Before
@@ -32,14 +41,22 @@ public class ShopTest {
                 Make.BCRICH, "G756", 6, true, 200, 300, guitarString);
 
         bassDrum = new BassDrum("Bass Drum", 150, 275);
+        bassDrum2 = new BassDrum("Bass Drum", 150, 275);
         cymbal = new Cymbal("Cymbal", 50, 75);
+        cymbal2 = new Cymbal("Cymbal", 50, 75);
         snare = new Snare("Snare", 70, 100);
+        snare2 = new Snare("Snare", 70, 100);
         tomTomDrum = new TomTomDrum("Tom Tom Drum", 70, 150);
+        tomTomDrum2 = new TomTomDrum("Tom Tom Drum", 70, 150);
         drumStick = new DrumStick("Drum Stick", 10, 20);
         drumKit = new DrumKit("Drum Kit", "Oak", Colour.BLUE, Type.PERCUSSION, Make.TAMA, "Tiger Mark 7", drumStick);
 
         keyBoard = new KeyBoard("Keyboard", "plastic", Colour.RED, Type.KEYBOARD, Make.ROLAND, "ty6788", 91,
                 200, 300);
+
+        sheetMusic = new SheetMusic("Sheet Music", 2, 6);
+
+        shop = new Shop("Rays Music Store");
 
         drumKit.addDrum(bassDrum);
         drumKit.addDrum(tomTomDrum);
@@ -63,5 +80,66 @@ public class ShopTest {
         assertEquals(105, guitar.calculateMarkup(), 0.1);
     }
 
+    @Test
+    public void canGetStockSize(){
+        assertEquals(0, shop.numberOfItemsInStock());
+    }
 
+    @Test
+    public void canAddItemstoStock(){
+        shop.addStock(guitar);
+        assertEquals(1, shop.numberOfItemsInStock());
+    }
+
+    @Test
+    public void canRemoveItemsFromStock(){
+        shop.addStock(guitar);
+        assertEquals(1, shop.numberOfItemsInStock());
+        shop.removeStock(guitar);
+        assertEquals(0, shop.numberOfItemsInStock());
+    }
+
+    @Test
+    public void canCalculateTotalProfit(){
+        shop.addStock(guitar);
+        shop.addStock(drumKit);
+        shop.addStock(keyBoard);
+        shop.addStock(bassDrum2);
+        shop.addStock(snare2);
+        shop.addStock(tomTomDrum2);
+        shop.addStock(cymbal2);
+        shop.addStock(sheetMusic);
+        assertEquals(8, shop.numberOfItemsInStock());
+        assertEquals(739, shop.calculateTotalPotentialProfit(), 0.1);
+    }
+
+    @Test
+    public void canCalculateTotalBuyPrice() {
+        shop.addStock(guitar);
+        shop.addStock(drumKit);
+        shop.addStock(keyBoard);
+        shop.addStock(bassDrum2);
+        shop.addStock(snare2);
+        shop.addStock(tomTomDrum2);
+        shop.addStock(cymbal2);
+        shop.addStock(sheetMusic);
+        assertEquals(8, shop.numberOfItemsInStock());
+        assertEquals(1097, shop.calculateTotalBuyPrice(), 0.1);
+
+    }
+
+    @Test
+    public void canCalculateTotalSellPrice() {
+        shop.addStock(guitar);
+        shop.addStock(drumKit);
+        shop.addStock(keyBoard);
+        shop.addStock(bassDrum2);
+        shop.addStock(snare2);
+        shop.addStock(tomTomDrum2);
+        shop.addStock(cymbal2);
+        shop.addStock(sheetMusic);
+        assertEquals(8, shop.numberOfItemsInStock());
+        assertEquals(1836, shop.calculateTotalSellPrice(), 0.1);
+
+    }
 }
